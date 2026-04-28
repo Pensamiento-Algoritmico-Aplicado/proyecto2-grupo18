550b94d549fb8dcb8da38fb5c544ec247b3000a2
Baltazar-Campino <bcampino@miuandes.cl>
2026-04-28 10:51:19 -0400

    agrupar datos filtrados en listas ordenadas por timestamp

--- procesar.py
+++ procesar.py
@@ -11,6 +11,10 @@ def ejecutar():
     if len(sys.argv) < 2: return
     archivo_in = sys.argv[1]
     
+    sectores = {}
+    sensores = {}
+    map_sec_sen = {}
+
     with open(archivo_in, 'r', encoding='utf-8', buffering=16*1024*1024) as f:
         for linea in f:
             partes = linea.strip().split('|')
@@ -25,6 +29,21 @@ def ejecutar():
                 if tipo == 'TEMP' and not (-10.0 <= v_num <= 60.0): continue
                 
                 t_int = ts_a_int(ts_str)
+
+                k_sec = (sec, tipo)
+                if k_sec not in sectores:
+                    sectores[k_sec] = (array.array('q'), array.array('d'))
+                sectores[k_sec][0].append(t_int)
+                sectores[k_sec][1].append(v_num)
+
+                k_sen = (s_id, tipo)
+                if k_sen not in sensores:
+                    sensores[k_sen] = (array.array('q'), array.array('d'))
+                sensores[k_sen][0].append(t_int)
+                sensores[k_sen][1].append(v_num)
+
+                if sec not in map_sec_sen: map_sec_sen[sec] = set()
+                map_sec_sen[sec].add(s_id)
             except Exception:
                 continue
 
